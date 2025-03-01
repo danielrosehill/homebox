f16a0cd5f2d2694b8b5e5e3aba3bbe3285118a11
Daniel Rosehill <daniel@danielrosehill.co.il>
2025-03-01 23:23:21 +0200

    Fix Docker build issues by providing default values for build arguments

--- docker/testing-favorite-items/build-and-run.sh
+++ docker/testing-favorite-items/build-and-run.sh
@@ -5,9 +5,18 @@
 
 set -e
 
+# Set build arguments
+COMMIT=$(git rev-parse --short HEAD)
+BUILD_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+VERSION="dev-favorites"
+
 # Build the Docker image
 echo "Building Docker image for favorites feature testing..."
-docker build -t danielrosehill/homebox:favorite-items -f Dockerfile ../..
+docker build \
+  --build-arg COMMIT="$COMMIT" \
+  --build-arg BUILD_TIME="$BUILD_TIME" \
+  --build-arg VERSION="$VERSION" \
+  -t danielrosehill/homebox:favorite-items -f Dockerfile ../..
 
 # Run the Docker container
 echo "Running Docker container for favorites feature testing..."
